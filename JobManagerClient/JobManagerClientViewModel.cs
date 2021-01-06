e772adbbac095ad2c34c419e25c11660f64d50d4
raczandras <raczandras0204@gmail.com>
2021-01-06 12:08:40 +0100

    Created a basic viewmodel for the job manager client

---
+++ JobManagerClient/JobManagerClientViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.VisualStudio.PlatformUI;
+
+
+namespace JobManagerClient
+{
+    public class JobManagerClientViewModel
+    {
+        public ICommand AddJobCommand { get; }
+
+        public string SelectedJob { get; set; }
+
+        public string name { get; set; }
+
+        public string carType { get; set; }
+
+        public string licensePlate { get; set; }
+
+        public string faliure { get; set; }
+
+        public ObservableCollection<Job> Jobs { get; }
+
+        private void AddJob()
+        {
+            Job job = new Job(name, carType, licensePlate, faliure);
+            Jobs.Insert(0,job);
+        }
+
+        public JobManagerClientViewModel()
+        {
+            Jobs = new ObservableCollection<Job>();
+            AddJobCommand = new DelegateCommand(AddJob);
+        }
+
+    }
+}
