552115d0edc70db6ef17a6e2613c2670bed59f84
raczandras <raczandras0204@gmail.com>
2021-01-06 12:08:07 +0100

    Created the data class for the jobs

---
+++ JobManagerClient/Job.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManagerClient
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; }
+
+        public string Name { get; set; }
+
+        public string CarType { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public string Failure { get; set; }
+
+        public Job(int id, DateTime date, string name, string carType, string licensePlate, string failure)
+        {
+            Id = id;
+            Date = date;
+            Name = name;
+            CarType = carType;
+            LicensePlate = licensePlate;
+            Failure = failure;
+        }
+
+        public Job(string name, string carType, string licensePlate, string failure)
+        {
+            Id = 0;
+            Date = DateTime.Now;
+            Name = name;
+            CarType = carType;
+            LicensePlate = licensePlate;
+            Failure = failure;
+        }
+
+
+    }
+}
