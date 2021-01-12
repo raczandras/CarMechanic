92d348889bc9b05aa69d36bdfd19b1580fc80ff7
raczandras <raczandras0204@gmail.com>
2021-01-12 13:39:03 +0100

    Finished the tests for the name validation

--- JobManagerClient.Test/DataValidatorTests.cs
+++ JobManagerClient.Test/DataValidatorTests.cs
@@ -62,7 +62,88 @@ namespace JobManagerClient.Test
             Assert.IsFalse(dataValidator.checkLicensePlate(licensePlate));
         }
 
-       
+        [TestMethod]
+        public void TrimNameCorrectlyOnePlusSpace()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var actual = dataValidator.correctName(" Correct Name ");
+            var expected = "Correct Name";
+
+            //Act and assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TrimNameCorrectlyManyPlusSpaces()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var actual = dataValidator.correctName("      Correct Name                  ");
+            var expected = "Correct Name";
+
+            //Act and assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UppercaseNameCorrectlyTwoParts()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var actual = dataValidator.correctName("correct name");
+            var expected = "Correct Name";
+
+            //Act and assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UppercaseNameCorrectlyManyParts()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var actual = dataValidator.correctName("looks like someone has a really long name");
+            var expected = "Looks Like Someone Has A Really Long Name";
+
+            //Act and assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Name_WithoutSpace_ShouldReturnFalse()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var name = "nospace";
+
+            //Act and assert
+            Assert.IsFalse(dataValidator.checkName(name));
+        }
+
+        [TestMethod]
+        public void Name_WithOneSpace_ShouldReturnTrue()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var name = "is space";
+
+            //Act and assert
+            Assert.IsTrue(dataValidator.checkName(name));
+            //Act and assert
+        }
+
+        [TestMethod]
+        public void Name_WithSeveralSpaces_ShouldReturnTrue()
+        {
+            //Arrange
+            DataValidator dataValidator = new DataValidator();
+            var name = "looks like someone has a really long name";
+
+            //Act and assert
+            Assert.IsTrue(dataValidator.checkName(name));
+            //Act and assert
+        }
 
     }
 }
