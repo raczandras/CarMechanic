7f30b763b3b84af1bcaf8651a8f2e966909b868e
matravolgyiadrian <matravolgyiadrian@gmail.com>
2021-01-15 21:08:47 +0100

    Implemented repository for the mechanic client project

---
+++ JobManager_Server/Repositories/MechanicClientRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MechanicClient;
+
+namespace JobManager_Server.Repositories
+{
+    public class MechanicClientRepository
+    {
+        private const string filename = "Jobs.json";
+
+        public static IEnumerable<Job> GetJobs()
+        {
+            if (File.Exists(filename))
+            {
+                var rawData = File.ReadAllText(filename);
+                var jobs = JsonSerializer.Deserialize<IEnumerable<Job>>(rawData);
+                return jobs;
+            }
+
+            return new List<Job>();
+        }
+
+        public static void StoreJobs(IEnumerable<Job> job)
+        {
+            var rawData = JsonSerializer.Serialize(job);
+            File.WriteAllText(filename, rawData);
+        }
+    }
+}
